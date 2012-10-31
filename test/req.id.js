52842a78bc482af613986ea0debd72de9c3528ac
Jake Luer <jake.luer@gmail.com>
2012-10-30 21:30:22 -0400

    tests/req.id: use spyies

--- test/req.id.js
+++ test/req.id.js
@@ -3,54 +3,58 @@ describe('req', function () {
   describe('id', function () {
 
     it('can be provided by header x-request-id', function (done) {
-      var app = hyperion();
+      var app = hyperion()
+        , spy = chai.spy(function (req, res) {
+            req.should.have.header('x-request-id');
+            req.should.have.property('id');
+            req.id.should.equal('hello-universe');
 
-      app.use(function (req, res) {
-        req.should.have.header('x-request-id');
-        req.should.have.property('id');
-        req.id.should.equal('hello-universe');
+            // TODO: automatic handling of such things
+            res.writeHeader(200, { 'content-type': 'text/plain' });
+            res.write(req.id);
+            res.end();
+          });
 
-        // TODO: automatic handling of such things
-        res.writeHeader(200, { 'content-type': 'text/plain' });
-        res.write(req.id);
-        res.end();
-      });
+      app.use(spy);
 
       chai.request(app)
-        .get('/')
-        .req(function (req) {
-          req.set('X-Request-Id', 'hello-universe');
-        })
-        .res(function (res) {
-          res.should.have.status(200);
-          res.should.be.text;
-          res.text.should.equal('hello-universe');
-          done();
-        });
+      .get('/')
+      .req(function (req) {
+        req.set('X-Request-Id', 'hello-universe');
+      })
+      .res(function (res) {
+        res.should.have.status(200);
+        res.should.be.text;
+        res.text.should.equal('hello-universe');
+        spy.should.have.been.called.once;
+        done();
+      });
     });
 
     it('can be generate if not in header', function (done) {
-      var app = hyperion();
+      var app = hyperion()
+        , spy = chai.spy(function (req, res) {
+            req.should.not.have.header('x-request-id');
+            req.should.have.property('id');
+            req.id.should.have.length(36);
 
-      app.use(function (req, res) {
-        req.should.not.have.header('x-request-id');
-        req.should.have.property('id');
-        req.id.should.have.length(36);
+            // TODO: automatic handling of such things
+            res.writeHeader(200, { 'content-type': 'text/plain' });
+            res.write(req.id);
+            res.end();
+          });
 
-        // TODO: automatic handling of such things
-        res.writeHeader(200, { 'content-type': 'text/plain' });
-        res.write(req.id);
-        res.end();
-      });
+      app.use(spy);
 
       chai.request(app)
-        .get('/')
-        .res(function (res) {
-          res.should.have.status(200);
-          res.should.be.text;
-          res.text.should.have.length(36);
-          done();
-        });
+      .get('/')
+      .res(function (res) {
+        res.should.have.status(200);
+        res.should.be.text;
+        res.text.should.have.length(36);
+        spy.should.have.been.called.once;
+        done();
+      });
     });
 
   }); // id
