12db8ea2738ee83c204b581bcd124b680e86e9a5
Jake Luer <jake.luer@gmail.com>
2012-11-08 06:56:06 -0500

    add dragonfly based http errors, import all error codes
    - dragonfly dependancy
    - lib/hyperion/errors.js error creation util
    - tests for errors

---
+++ test/errors.js
@@ -0,0 +1,23 @@
+var STATUS_CODES = require('http').STATUS_CODES
+  , errors = __hyperion.errors;
+
+describe('errors', function () {
+  it('should imported all http errors', function () {
+    var codeKeys = Object.keys(STATUS_CODES)
+      , errorKeys = Object.keys(errors._specs)
+    errorKeys.should.have.length(codeKeys.length);
+  });
+
+  it('should create an error with correct properties', function () {
+    (function () {
+      throw errors.create('internal server error');
+    }).should.throw(errors._proto, 'Internal Server Error');
+  });
+
+  it('should be able to get the http status from an error', function () {
+    var err = errors.create('internal server error')
+      , json = err.toJSON();
+    err.should.have.property('status', '500');
+    json.should.have.property('status', '500');
+  });
+});
