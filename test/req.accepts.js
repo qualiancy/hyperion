b8dab2333dc066512a4b2c8f926d87195b04a02d
Jake Luer <jake.luer@gmail.com>
2012-11-08 12:03:48 -0500

    request: improve accept header parsing
    - util: parseAccept includes type/subtype
    - request: accepts works with wildcards
    - tests for all scenarios

--- test/req.accepts.js
+++ test/req.accepts.js
@@ -6,6 +6,8 @@ describe('req', function () {
       app.use(function (req, res) {
         req.accepts('application/json').should.be.false;
         req.accepts('text/plain').should.be.true;
+        req.accepts('json').should.be.false;
+        req.accepts('text').should.be.true;
 
         res
         .status(200)
@@ -17,6 +19,34 @@ describe('req', function () {
       .get('/')
       .req(function (req) {
         req.set('accept', 'text/plain');
+      })
+      .res(function (res) {
+        res.should.have.status(200);
+        done();
+      });
+    });
+
+    it('should determine if req accepts wildcard type', function (done) {
+      var app = hyperion();
+
+      app.use(function (req, res) {
+        req.accepts('application/json').should.be.false;
+        req.accepts('text/plain').should.be.true;
+        req.accepts('text/html').should.be.true;
+        req.accepts('json').should.be.false;
+        req.accepts('text').should.be.true;
+        req.accepts('html').should.be.true;
+
+        res
+        .status(200)
+        .type('text')
+        .end('hello universe');
+      });
+
+      chai.request(app)
+      .get('/')
+      .req(function (req) {
+        req.set('accept', 'text/*');
       })
       .res(function (res) {
         res.should.have.status(200);
--- test/util.parseAccepts.js
+++ test/util.parseAccepts.js
@@ -5,15 +5,20 @@ describe('util', function () {
     it('should parse a basic accept statement', function () {
       var parsed = util.parseAccept('application/json');
       parsed.should.be.an('array');
-      parsed.should.deep.equal([{ value: 'application/json', quality: 1 }]);
+      parsed.should.deep.equal([{
+          value: 'application/json'
+        , quality: 1
+        , type: 'application'
+        , subtype: 'json'
+      }]);
     });
 
     it('should parse a multiple accept statement', function () {
       var parsed = util.parseAccept('text/plain, text/html');
       parsed.should.be.an('array');
       parsed.should.deep.equal([
-          { value: 'text/plain', quality: 1 }
-        , { value: 'text/html', quality: 1 }
+          { value: 'text/plain', quality: 1, type: 'text', subtype: 'plain' }
+        , { value: 'text/html', quality: 1, type: 'text', subtype: 'html' }
       ]);
     });
 
@@ -21,8 +26,8 @@ describe('util', function () {
       var parsed = util.parseAccept('text/plain; q=.5, text/html');
       parsed.should.be.an('array');
       parsed.should.deep.equal([
-          { value: 'text/html', quality: 1 }
-        , { value: 'text/plain', quality: 0.5 }
+          { value: 'text/html', quality: 1, type: 'text', subtype: 'html' }
+        , { value: 'text/plain', quality: 0.5, type: 'text', subtype: 'plain' }
       ]);
     });
   });
